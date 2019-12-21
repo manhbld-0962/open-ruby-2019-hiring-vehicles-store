8496c49e1089acfbaf16004da8b46f3bf68250a7
gdhaison <vuhaisonhb456@gmail.som>
2019-12-22 00:16:13 +0700

    Add template for User

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @vehicles = Vehicle.select(:id, :name, :price, :description).last Settings.newest_vehicle
+  end
 end
